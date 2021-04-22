2cdef8baf19005c01bd1e16518aa1e2a29558f17
Jnmendza <jonatmendoza@gmail.com>
2021-04-21 20:07:38 -0700

    updated cta on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,14 @@ function Header() {
     return (
         <div className="header-image-container">
                 <img className="header-image" src="/assets/Hero-image.jpg" alt="pool-image" />
-                <div className="cta">
-                    <p>Get A</p>
-                    <p>Free</p>
-                    <p>Estimate Today</p>
-                    <p>(619) 852-4625</p>
+                <div className="flex-display cta">
+                    <img src="/assets/theO.svg" alt="logo" height="auto" width="350px"/>
+                    <div style={{margin: "0 35px"}}>
+                        <p>Get A</p>
+                        <p>Free</p>
+                        <p>Estimate Today</p>
+                        <p>(619) 852-4625</p>
+                    </div>
                 </div>
         </div>
     )
